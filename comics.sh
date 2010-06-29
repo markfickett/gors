169d20d503428824ec1f6882e3e140cf42e60294
Mark Fickett <mark.fickett@gmail.com>
2010-06-28 21:32:42 -0700

    Update comics.sh. (This replaces what was a .bkp with what was the current version.)

--- comics.sh
+++ comics.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Check for comic updates and open web page if new
-# Generalized to check for any image-based comic where the first image
-# with a src=".." that follows a predictable form is the important update
+# Generalized from checking for any image-based comic where the important update
+# is the first image with a src="something" that follows a predictable form.
 # Uses various temporary files of the form [comicname][xyz]
 
 # Written by Mark Fickett [ naib.webhop.org ]
-# Last updated 2007 Oct 29
+# Last updated 2008 March 09
 
 ##### Configuration #####
 
@@ -15,62 +15,121 @@ OPENCMD='open'
 
 # Debug? (Any non-null value is 'false')
 # prints out received/interpreted arguments, leaves reformatted html
-# DEBUG='n'
+DEBUG='n'
 
 ##### Help text #####
 
-# The regexp can get expanded when just by itself;
-# let it, but remove spaces so it doesn't look like multiple arguments to test
-if test -z `echo $3 | tr -d ' '`
-then
-	echo ""
-	echo "Usage: comics.sh comicName comicPageURL ImageRegExp \\"
-	echo "	[tag attribute] [occurenceNumber]"
-	echo "For example,"
-	echo "comics.sh qwantz http://qwantz.com \\"
-	echo "	'http:\\/\\/www\\.qwantz\\.com\\/comics\\/comic.*\\.png'"
-	echo "or"
-	echo "comics.sh drmcninja http://drmcninja.com/ \\"
-	echo "	'http:\/\/drmcninja\.com\/page\.php\?pageNum=.*\&issue=.*' \\"
-	echo "	a href 2"
-	echo ""
-	echo "Various files of the form [comicname][xyz] will be used"
-	echo "temporarily, and the file [comicname]old will be used to store"
-	echo "the latest available image."
-	echo ""
-	exit
-fi
+function printHelpAndExit() {
+cat - <<EOF
+Argumens:
+	-n
+	--name comicName (required)
+	-u
+	--url comicPageURL (required)
+	-r
+	--regexp ImageRegExp (required)
+	-t
+	--tag tag (optional, default is img)
+	-a
+	--attribute attribute (optional, default is src)
+	-N
+	--occurence occurenceNumber (optional, default is 1)
+	-o
+	--openattribute (optional, default is false)
+For example:
+	comics.sh --name qwantz --url http://qwantz.com \\
+		'http:\\/\\/www\\.qwantz\\.com\\/comics\\/comic.*\\.png'
+	or
+	comics.sh drmcninja http://drmcninja.com/ \\
+		'http:\/\/drmcninja\.com\/page\.php\?pageNum=.*\&issue=.*' \\
+		a href 2
+Various files of the form [comicname][xyz] will be used
+	temporarily, and the file [comicname]old will be used to store
+	the url (attribute value) of the latest available image (or other item).
+EOF
+	echo Original call was: $*
+	exit 1
+}
 
-##### Initial setup #####
+##### Argument Parsing #####
 
 # for unique storage of temporary data
-COMICNAME=$1
-
+COMICNAME=''
 # for fetching the page to look through
-COMICADDRESS=$2
-
+COMICADDRESS=''
 # a regexp which will match the source for the image of the most recent comic
-COMICIMGRE=/$3/p
+COMICIMGRE=''
 
-if test -z $4
-then
-	TAG=img
-else
-	TAG=$4
-fi
+# what tag to look for
+TAG=img
+# what attribute to look for, in the tag
+ATTR=src
+# what ordinal occurance of the tag to look for
+NUM=1
+# if set, open the retrived attribute value, rather than the COMICADDRESS
+# OPENATTRIBUTE
 
-if test -z $5
-then
-	ATTR=src
-else
-	ATTR=$5
-fi
+while [ $# -gt 0 ]
+do
+	# All arguments require an argument, except "--openattribute"
+	if [ \( $# -lt 2 \) -a \( $1 != "--openattribute" \) ]
+	then
+		echo Error during argument parsing at
+		echo argc=$#
+		echo 1=$1
+		echo 2=$2
+		printHelpAndExit $0 $*
+	fi
 
-if test -z $6
+	case $1 in
+	"--name" | "-n" )
+		COMICNAME=$2
+		shift
+		;;
+	"--url" | "-u" )
+		COMICADDRESS=$2
+		shift
+		;;
+	"--regexp" | "-r" )
+		COMICIMGRE=/$2/p
+		shift
+		;;
+	"--tag" | "-t" )
+		TAG=$2
+		shift
+		;;
+	"--attribute" | "-a" )
+		ATTR=$2
+		shift
+		;;
+	"--occurence" | "-N" )
+		NUM=$2
+		shift
+		;;
+	"--openattribute" | "-o" )
+		OPENATTRIBUTE="YES"
+		;;
+	* )
+		echo "Unrecognized or misplaced argument: " $1
+		printHelpAndExit $0 $*
+		;;
+	esac
+	shift
+done
+
+if [ -z "$COMICNAME" ]
 then
-	NUM=1
-else
-	NUM=$6
+	echo "Required argument --name not given"
+	printHelpAndExit $0 $*
+elif [ -z "$COMICADDRESS" ]
+then
+	echo "Required argument --url not given"
+	printHelpAndExit $0 $*
+elif [ -z "$COMICIMGRE" ]
+then
+	echo "Required argument --regexp not given"
+	printHelpAndExit $0 $*
+	
 fi
 
 if test -z ${DEBUG}
@@ -81,8 +140,16 @@ then
 	echo '	' ${COMICIMGRE}
 	echo tag is: ${TAG}
 	echo attribute is: ${ATTR}
+	if [ "$OPENATTRIBUTE" ]
+	then
+		echo openattribute is yes
+	else
+		echo openattribute is no
+	fi
 	exit 0
 fi
+
+##### Initial setup #####
 
 # Flag as in-progress
 touch ${COMICNAME}going
@@ -158,6 +225,7 @@ rm ${COMICNAME}new
 if test -z ${DEBUG}
 then
 	echo ${COMICNAME}: current: ${CURRENT}
+	echo Debug exit: leaving ${COMICNAME}going
 	exit;
 fi
 
@@ -182,7 +250,12 @@ then
 	then
 		echo '     New '${COMICNAME}'!'
 		echo ${CURRENT} > ${COMICNAME}old
-		${OPENCMD} ${COMICADDRESS} &
+		if [ "${OPENATTRIBUTE}" ]
+		then
+			${OPENCMD} ${CURRENT} &
+		else
+			${OPENCMD} ${COMICADDRESS} &
+		fi
 		OPENRET=$?
 		if [ $OPENRET != 0 ]
 		then
@@ -194,7 +267,12 @@ then
 else
 	echo '  No old '${COMICNAME}'.'
 	echo ${CURRENT} > ${COMICNAME}old
-	${OPENCMD} ${COMICADDRESS} &
+	if [ "${OPENATTRIBUTE}" ]
+	then
+		${OPENCMD} ${CURRENT} &
+	else
+		${OPENCMD} ${COMICADDRESS} &
+	fi
 fi
 
 # Remove in-progress flag
